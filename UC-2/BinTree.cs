b6f1e4fd0014ecbc8281bdce034fae54b9871ce1
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-03-11 21:15:41 +0530

    adding program file , node of 2nd problem

---
+++ UC-2/BinTree.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinTree
+{
+    public class binaryTree
+    {
+        public Node Root { get; set; }
+        public bool Add(int value)
+        {
+         Node before = null, after = this.Root;
+   
+            while (after != null)
+            {
+                before = after;
+
+                if (value < after.Data)
+                {
+                    after = after.LeftNode;
+                }
+
+                else if (value > after.Data)
+                {
+                    after = after.RightNode;
+                }
+
+                else
+                {
+                    return false;
+                }
+            }
+            Node newNode = new Node();
+            newNode.Data = value;
+
+            if (this.Root == null)
+            {
+                this.Root = newNode;
+            }
+
+            else
+            {
+                if (value < before.Data)
+                    before.LeftNode = newNode;
+                else
+                    before.RightNode = newNode;
+            }
+            return true;        }
+
+        public void TraversePreorder(Node Parent)
+        {
+            if (Parent != null)
+            {
+                Console.WriteLine(Parent.Data + "");
+                Console.WriteLine("------------------------------ Left node");
+                TraversePreorder(Parent.LeftNode);
+                Console.WriteLine("------------------------------ Right node");
+                TraversePreorder(Parent.RightNode);
+
+            }
+        }
+
+    }
+}
---
+++ UC-2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BinTree;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        binaryTree b = new binaryTree();
+
+        b.Add(56);
+        b.Add(30);
+        b.Add(40);
+        b.Add(20);
+        b.Add(11);
+        b.Add(3);
+        b.Add(16);
+        b.Add(95);
+        b.Add(70);
+
+        b.Add(65);
+        b.Add(63);
+        b.Add(67);
+        b.Add(70);
+
+        b.TraversePreorder(b.Root);
+    }
+}
