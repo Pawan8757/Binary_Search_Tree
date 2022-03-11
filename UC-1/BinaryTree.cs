89356a51cbeae5406b9e7a5343da039e60750ce3
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-03-11 21:12:55 +0530

    adding 1st problem

---
+++ UC-1/BinaryTree.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class BinaryTree
+    {
+        public Node Root { get; set; }
+        public bool Add(int value)
+
+        {
+            Node before = null, after = this.Root;
+
+            while(after != null)
+            {
+                before = after;
+
+            if(value<after.Data)
+                {
+                    after = after.LeftNode;
+                }
+             else if (value > after.Data)
+                {
+                    after = after.RightNode;
+                }
+             else
+                {
+                    return false;
+                }
+            }
+
+            Node NewNode = new Node();
+            NewNode.Data = value;
+
+            if (this.Root == null)
+            {
+                this.Root = new Node();
+            }
+            else
+            {
+                if (value < before.Data)
+                    before.LeftNode = NewNode;
+                else
+                    before.RightNode = NewNode;
+            }
+            return true;
+        }
+            public void TravarsePreorder(Node Parent)
+        {
+            if(Parent!=null)
+            {
+                Console.WriteLine(Parent.Data + " ");
+                TravarsePreorder(Parent.LeftNode);
+                TravarsePreorder(Parent.RightNode);
+            }
+        }
+    }
+}
---
+++ UC-1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree binaryTree = new BinaryTree();
+
+            binaryTree.Add(6);
+            binaryTree.Add(1);
+            binaryTree.Add(5);
+            binaryTree.Add(7);
+            binaryTree.Add(9);
+
+            binaryTree.TravarsePreorder(binaryTree.Root);
+
+        }
+    }
+}
